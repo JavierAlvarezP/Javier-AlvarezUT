4f5676fb14db336113109ff4222202cc3e1e9d9a
JavierAlvarezP <56983796+JavierAlvarezP@users.noreply.github.com>
2019-10-30 19:45:36 -0600

    Add files via upload

---
+++ Promedio.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#promedio 
+suma=0
+
+n1=int(input("ingrese la primer edad:"))
+n2=int(input("ingrese la segunda edadr:"))
+n3=int(input("ingrese la tercera edad:"))
+
+prom=(n1+n2+n3)/3
+
+print("La edad promedio es:",prom)
+
+
+# In[ ]:
+
+
+
+
